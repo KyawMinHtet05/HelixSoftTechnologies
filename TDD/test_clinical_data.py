c346b7bfc28784394fbaf35adae3134117d26098
Kyaw Min Htet <zane4ethnic@gmail.com>
2026-01-16 11:26:54 +0630

    fix

--- TDD/test_clinical_data.py
+++ TDD/test_clinical_data.py
@@ -1,12 +1,12 @@
-# import pytest
-# import tempfile
-# import os
-# from pathlib import Path
+import pytest
+import tempfile
+import os
+from pathlib import Path
 
-# def test_clinical_data_processor_connection():
-#     processor = ClinicalDataProcessor("localhost", "test", "test")
-#     assert processor is not None  
+def test_clinical_data_processor_connection():
+    processor = ClinicalDataProcessor("localhost", "test", "test")
+    assert processor is not None  
 
-# def test_clinical_data_validator_creation():
-#     validator = ClinicalDataValidator("temp", "temp", "temp")
-#     assert validator is not None
+def test_clinical_data_validator_creation():
+    validator = ClinicalDataValidator("temp", "temp", "temp")
+    assert validator is not None
